51558c85d4034ce43e38683296215b4d01eb0976
motailab <mdmotailab@gmail.com>
2017-10-30 15:28:24 +0600

    Added Our First ViewSet and router

--- src/profile_project/profiles_api/urls.py
+++ src/profile_project/profiles_api/urls.py
@@ -1,6 +1,12 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
+
 from . import views
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewset, base_name='hello-viewset')
 
 urlpatterns = [
     url(r'^hello-view/', views.HelloApi.as_view(), name='hello-view'),
+    url(r'', include(router.urls)),
 ]
--- src/profile_project/profiles_api/views.py
+++ src/profile_project/profiles_api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response 
 from rest_framework import status
@@ -7,6 +8,21 @@ from rest_framework import status
 from . import serializers
 
 # Create your views here.
+class HelloViewset(viewsets.ViewSet):
+    """ Test Viewset api """
+
+    def list(self, request):
+        """Return a hello meassge."""
+
+        a_viewset = [
+            'Uses actions (list, create, retrive, update, partial_update)',
+            'Automatically maps to URLs usign Routers',
+            'provide more functionality with less code',
+        ]
+
+        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+
+    
 class HelloApi(APIView):
     """Test Api View."""
 
