261f671fcf4958ddd95f5035ec891004e8cb3684
motailab <mdmotailab@gmail.com>
2017-10-30 20:38:02 +0600

    Added permissions and authentication to our UserProfileViewSet.

---
+++ src/profile_project/profiles_api/permissions.py
@@ -0,0 +1,16 @@
+from rest_framework import permissions
+
+
+class UpdateOwnProfile(permissions.BasePermission):
+    """Allow users to update their own profile."""
+
+    def has_object_permission(self, request, view, obj):
+        """Check user trying to update their own profile."""
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.id == request.user.id
+    
+
+            
